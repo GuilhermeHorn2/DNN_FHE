f95f4fd357cd96270506c1679927195763f96065
mauricioLeite <mauricioleite.fe@gmail.com>
2026-05-04 16:42:06 -0300

    refactor: organize code

---
+++ Chebyshev/build/CMakeFiles/dinn_inference.dir/DependInfo.cmake
@@ -0,0 +1,19 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/efm9ca/projects/DNN_FHE/Chebyshev/activation_func_aprox.cpp" "CMakeFiles/dinn_inference.dir/activation_func_aprox.cpp.o" "gcc" "CMakeFiles/dinn_inference.dir/activation_func_aprox.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
